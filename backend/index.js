31819f7fccfd7b7fe951ba65ff425329a4a27cb7
Sean Z <contact@seanzeng.com>
2019-08-15 05:53:06 +1200

    sockets working

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const https = require('https');
 const http = require('http');
+const socketIO = require('socket.io');
+const axios = require('axios');
+
+/*
+ Need Swagger
+ */
 
 const app = express();
 
 const HTTP_PORT = 8080;
-const HTTPS_PORT = null; // 443;
+// const HTTPS_PORT = null; // 443;
 const HOST = '0.0.0.0';
 
 let active = false;
 
 const api = express.Router();
+
 api.get('/', (req, res) => {
     res.send('api v1.0');
 });
@@ -30,12 +37,39 @@ app.get('/', (req, res) => {
 app.use('/api', api);
 
 
-http.createServer(app).listen(HTTP_PORT, HOST, () => {
+// Testing Socket.io
+const getApiAndEmit = async socket => {
+    try {
+        const res = await axios.get(
+            "https://api.darksky.net/forecast/e31bf4cadf43d410d2da84139ab49ab2/-36.846773,174.763057"
+        ); // Getting the data from DarkSky
+        socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+        console.error(`Error: ${error.code}`);
+    }
+};
+
+const httpServer = http.createServer(app);
+const io = socketIO(httpServer);
+io.on("connection", socket => {
+    console.log("New client connected");
+    const clientUpdater = setInterval(
+        () => getApiAndEmit(socket),
+        10000
+    );
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        clearInterval(clientUpdater);
+    });
+});
+httpServer.listen(HTTP_PORT, HOST, () => {
     console.log(`HTTP listening on ${HTTP_PORT}`)
 });
-if (isNaN(HTTPS_PORT)) {
-    https.createServer(app).listen(HTTPS_PORT, HOST, () => {
-        console.log(`HTTPS listening on ${HTTPS_PORT}`);
-    })
-}
+
+
+// if (isNaN(HTTPS_PORT)) {
+//     https.createServer(app).listen(HTTPS_PORT, HOST, () => {
+//         console.log(`HTTPS listening on ${HTTPS_PORT}`);
+//     })
+// }
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ScreenSaver from "./components/ScreenSaver/ScreenSaver";
 import './App.module.css';
+import socketIOClient from "socket.io-client";
 
 const App: React.FC = () => {
 
     const [screenSaver, setScreenSaver] = useState(true);
+    const [weatherEndpoint] = useState('http://127.0.01:8080');
+    const [weatherData, setWeather] = useState('');
 
     const toggleScreenSaver = () => setScreenSaver(!screenSaver);
 
+    useEffect(() => {
+        console.log('run once');
+        const socket = socketIOClient(weatherEndpoint);
+        socket.on("FromAPI", (data:string) => {
+            setWeather(data);
+        })
+    }, []);
+
     return (
         <div className="App">
+            {weatherData}
             {screenSaver ? <ScreenSaver onClick={toggleScreenSaver}/> : null}
+            {!screenSaver ? <button onClick={toggleScreenSaver} className={"fa fa-pause"}/> : null}
         </div>
     );
 };
