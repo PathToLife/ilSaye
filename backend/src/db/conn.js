315941048cdb23cfe5f5aeafece9b3d527822af4
Sean Z <contact@seanzeng.com>
2019-08-16 16:58:43 +1200

    db test complete

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,5 +1,7 @@
 const {Connection, Request} = require('tedious');
 
+const LOAD_RESULT_MEMORY = true;
+
 const config = {
     server: process.env.DB_HOST, // update me
     authentication: {
@@ -9,36 +11,19 @@ const config = {
         },
         type: 'default'
     },
-    options:
-        {
-            database: 'ilSayeDB', //update me
-            encrypt: true
-        }
+    options: {
+        database: 'ilSayeDB', //update me
+        encrypt: true,
+        rowCollectionOnRequestCompletion: LOAD_RESULT_MEMORY
+    }
 };
-
-function executeStatement() {
-    const request = new Request("select 42, 'hello world'", function (err, rowCount) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(rowCount + ' rows');
-        }
-    });
-
-    request.on('row', function (columns) {
-        columns.forEach(function (column) {
-            console.log(column.value);
-        });
-    });
-
-    connection.execSql(request);
-}
 
 const connection = new Connection(config);
 
 connection.on('connect', function (err) {
     // If no error, then good to go...
-    executeStatement();
+    if (err) console.log(err);
+    console.log(`DB Connected ${config.server}`)
 });
 
 module.exports = connection;
--- backend/src/db/conn.test.js
+++ backend/src/db/conn.test.js
@@ -1,15 +1,60 @@
 require('dotenv').config();
 
 const expect = require('expect');
+const {Request, TYPES} = require('tedious');
 
 describe("DB", () => {
 
-    it('should import without errors', () => {
-        require('./conn');
+    let conn = null;
+    it('should connect without errors', (done) => {
+        conn = require('./conn');
+        conn.on('connect', (err) => {
+            done(err);
+        });
+
     });
 
-    it('should be defined DB_HOST', () => {
+    it('should be defined DB_HOST', (done) => {
         const host = process.env.DB_HOST;
         expect(host).toBeDefined();
-    })
+        done();
+    });
+
+    it('should write to test_table', (done) => {
+        // language=TSQL
+        const stmt = `INSERT INTO dbo.test_table (text)
+                      VALUES (@text)`;
+        const request = new Request(stmt
+            , function (err, rowCount) {
+                if (err) {
+                    console.log(err);
+                    done(err);
+                } else {
+                    // console.log(`Inserted ${rowCount} Rows`);
+                    done();
+                }
+            });
+        request.addParameter('text', TYPES.VarChar, 'Mocha Test Text');
+
+        conn.execSql(request);
+    });
+
+    it('should download from test_table', (done) => {
+        // language=TSQL
+        const stmt = `SELECT * FROM dbo.test_table`;
+        const req = new Request(stmt, (err, rowCount, rows) => {
+            if (!err) {
+                console.log(`Got ${rowCount} rows`);
+                rows.forEach(row => {
+                    const data = row.map(col => col.value);
+                    console.log(data);
+                });
+                done();
+            } else {
+                done(err);
+            }
+        });
+
+        conn.execSql(req);
+    });
 });
