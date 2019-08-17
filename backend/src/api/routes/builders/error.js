522b010bca61d0a353f69599ae589cdf34f992d9
Sean Z <contact@seanzeng.com>
2019-08-18 08:57:48 +1200

    better error object creation

--- backend/src/api/routes/builders/error.js
+++ backend/src/api/routes/builders/error.js
@@ -1,15 +1,32 @@
+/**
+ * errors from this server are served in this json format
+ * {
+ *     errorCode: number,
+ *     msg: string
+ * }
+ * @param errorCode
+ * @param msg
+ * @returns {string}
+ */
 const jsonError = (errorCode, msg) => {
-    return JSON.stringify({errorCode, msg}, null, 2);
-};
-
-const sendNotImplemented = (res) => {
-    sendError(res, 501, 'not implemented')
+    let errorString = '';
+    try {
+        errorString = JSON.stringify({errorCode, msg}, null, 2);
+    } catch (e) {
+        errorString = JSON.stringify({errorCode, msg: "exception occurred and parsing error object failed"}, null, 2)
+    }
+    return errorString
 };
 
 const sendError = (res, code, message) => {
     res.status(code);
     res.send(jsonError(code, message));
 };
+
+const sendNotImplemented = (res) => {
+    sendError(res, 501, 'not implemented')
+};
+
 
 module.exports = {
     sendError,
